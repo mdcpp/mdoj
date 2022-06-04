23eb3f4dcdea2f8c6c1ef667241e58af4cdddc3c
KAIYOHUGO <41114603+KAIYOHUGO@users.noreply.github.com>
2022-06-04 13:24:21 +0000

    Fix

--- backend/src/controller/token.rs
+++ backend/src/controller/token.rs
@@ -1,6 +1,6 @@
 use crate::entity;
 use openssl::aes;
-use sea_orm::{EntityTrait, QueryFilter};
+use sea_orm::prelude::*;
 
 const AES_KEY: &[u8; 32] = include_bytes!["../../config/aes"];
 
@@ -11,8 +11,8 @@ pub struct AuthPayload<'a> {
 
 pub async fn generate<'a>(payload: AuthPayload<'a>) {
     entity::user_table::Entity::find()
-        .filter(entity::user_table::Column::name_user.eq(payload.username))
-        .filter(entity::user_table::Column::hashed_password.eq(payload.password));
+        .filter(entity::user_table::Column::NameUser.eq(payload.username))
+        .filter(entity::user_table::Column::HashedPassword.eq(payload.password));
 }
 pub async fn revoke() {}
 pub async fn verify() {}
