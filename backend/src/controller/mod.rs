f73069d1c01a3f540b5658ba01b7f5791dc07f95
eason <30045503+Eason0729@users.noreply.github.com>
2023-05-31 10:10:23 +0800

    change deps of backend

--- backend/src/controller/mod.rs
+++
@@ -1,4 +0,0 @@
-mod remove_token_2;
-pub mod role;
-pub mod token;
-pub mod user;
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,27 +1,3 @@
-use actix_web::{middleware, web::Data, App, HttpServer};
-mod config;
-mod controller;
-mod route;
-mod state;
-use std::env;
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    config::init().await;
-
-    let host = env::var("HOST").unwrap_or("0.0.0.0".to_owned());
-    let port = env::var("PORT")
-        .unwrap_or("8079".to_owned())
-        .parse::<u16>()
-        .unwrap();
-
-    HttpServer::new(move || {
-        App::new()
-            .wrap(middleware::Logger::default())
-            .app_data(Data::new(state::generate_state()))
-            .configure(route::configure)
-    })
-    .bind((host, port))?
-    .run()
-    .await
+fn main(){
+    
 }
