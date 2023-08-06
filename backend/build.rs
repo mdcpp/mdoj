cc157b6634bf66245786c9466320ccde2be389a9
eason <30045503+Eason0729@users.noreply.github.com>
2023-08-06 13:03:50 +0800

    fix proto

--- backend/build.rs
+++ backend/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("../proto/backend.proto")?;
-    tonic_build::compile_protos("../proto/sandbox.proto")?;
+    tonic_build::compile_protos("../proto/judger.proto")?;
     Ok(())
 }
--- backend/src/service/state.rs
+++ backend/src/service/state.rs
@@ -1,24 +1,24 @@
-use std::sync::Arc;
+// use std::sync::Arc;
 
-use crate::{init::config::CONFIG, proto::prelude::judge_service_client::JudgeServiceClient};
-use sea_orm::{Database, DatabaseConnection};
-use tonic::transport::Channel;
+// use crate::{init::config::CONFIG, proto::prelude::judge_service_client::JudgeServiceClient};
+// use sea_orm::{Database, DatabaseConnection};
+// use tonic::transport::Channel;
 
-struct ServiceState {
-    db: DatabaseConnection,
-    sandbox: JudgeServiceClient<Channel>,
-}
+// struct ServiceState {
+//     db: DatabaseConnection,
+//     sandbox: JudgeServiceClient<Channel>,
+// }
 
-impl ServiceState {
-    async fn new() -> Arc<ServiceState> {
-        let config = CONFIG.get().unwrap();
+// impl ServiceState {
+//     async fn new() -> Arc<ServiceState> {
+//         let config = CONFIG.get().unwrap();
 
-        let db = Database::connect(&config.database.uri).await.unwrap();
+//         let db = Database::connect(&config.database.uri).await.unwrap();
 
-        let sandbox = JudgeServiceClient::connect("http://127.0.0.1:8080")
-            .await
-            .unwrap();
+//         let sandbox = JudgeServiceClient::connect("http://127.0.0.1:8080")
+//             .await
+//             .unwrap();
 
-        Arc::new(ServiceState { db, sandbox })
-    }
-}
+//         Arc::new(ServiceState { db, sandbox })
+//     }
+// }
