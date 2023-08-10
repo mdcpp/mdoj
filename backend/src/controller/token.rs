5aa79235e495e4fbaa0bb48bbdc86a83620c8cad
eason <30045503+Eason0729@users.noreply.github.com>
2023-08-10 17:33:44 +0800

    draft Token

--- backend/src/controller/token.rs
+++ backend/src/controller/token.rs
@@ -1,15 +1,136 @@
+use chrono::{Duration, Local, NaiveDateTime};
+use entity::token;
+use lru::LruCache;
 use rand::Rng;
+use sea_orm::{ActiveModelTrait, ActiveValue, ColumnTrait, EntityTrait, QueryFilter};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::{num::NonZeroUsize, sync::Mutex};
 
+use crate::init::db::DB;
 
-pub struct TokenController{
+use super::util::bitflag::UserPermBytes;
+use super::Error;
+
+const EXPIRY_FRQU: usize = 10;
+type RAND = [u8; 16];
+
+macro_rules! report {
+    ($e:expr) => {
+        match $e {
+            Some(x) => x,
+            None => {
+                return Ok(None);
+            }x
+        }
+    };
+}
+
+#[derive(Clone)]
+struct CachedToken {
+    user_id: i32,
+    permission: i64,
+    expiry: NaiveDateTime,
+}
+
+impl From<token::Model> for CachedToken {
+    fn from(value: token::Model) -> Self {
+        Self {
+            user_id: value.user_id,
+            permission: value.permission,
+            expiry: value.expiry,
+        }
+    }
+}
+
+pub struct TokenController {
+    cache: Mutex<LruCache<RAND, CachedToken>>,
+    frqu: AtomicUsize,
+}
+
+impl Default for TokenController {
+    fn default() -> Self {
+        log::debug!("Setup TokenController");
+        let cache = Mutex::new(LruCache::new(NonZeroUsize::new(100).unwrap()));
+        Self {
+            cache,
+            frqu: Default::default(),
+        }
+    }
 }
 
 impl TokenController {
-    pub async fn add(&self,user_id:i32,token:Vec<u8>){
+    pub async fn add(&self, user: &entity::user::Model, dur: Duration) -> Result<String, Error> {
+        let db = DB.get().unwrap();
+
         let mut rng = rand::thread_rng();
         let rand: i128 = rng.gen();
-        let rand=rand.to_be_bytes();
-        // entity::token::ActiveModel{ id: todo!(), user_id: todo!(), rand: todo!() }
-        todo!();
+        let rand: RAND = rand.to_be_bytes();
+
+        let expiry = (Local::now() + dur).naive_local();
+
+        token::ActiveModel {
+            user_id: ActiveValue::Set(user.id),
+            rand: ActiveValue::Set(rand.to_vec().clone()),
+            expiry: ActiveValue::Set(expiry),
+            permission: ActiveValue::Set(user.permission),
+            ..Default::default()
+        }
+        .insert(db)
+        .await?;
+
+        Ok(hex::encode(rand))
+    }
+    pub async fn verify(&self, token: String) -> Result<Option<(i32, UserPermBytes)>, Error> {
+        let now = Local::now().naive_local();
+        let db = DB.get().unwrap();
+
+        if self.frqu.fetch_add(1, Ordering::Relaxed) % EXPIRY_FRQU == 1 {
+            tokio::spawn(
+                token::Entity::delete_many()
+                    .filter(token::Column::Expiry.lte(now))
+                    .exec(db),
+            );
+        }
+
+        let rand = report!(hex::decode(token).ok());
+        let rand: [u8; 16] = report!(rand.try_into().ok());
+
+        let token: CachedToken;
+
+        match {
+            let mut cache = self.cache.lock().unwrap();
+            match cache.get(&rand) {
+                Some(cc) => {
+                    if cc.expiry < now {
+                        cache.pop(&rand);
+                        None
+                    } else {
+                        Some(cc.clone())
+                    }
+                }
+                None => None,
+            }
+        } {
+            Some(token_) => {
+                token = token_;
+            }
+            None => {
+                token = report!(
+                    token::Entity::find()
+                        .filter(token::Column::Rand.eq(rand.to_vec()))
+                        .one(db)
+                        .await?
+                )
+                .into();
+
+                if token.expiry < now {
+                    return Ok(None);
+                }
+
+                self.cache.lock().unwrap().put(rand, token.clone());
+            }
+        }
+
+        Ok(Some((token.user_id, UserPermBytes(token.permission))))
     }
 }
