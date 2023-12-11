e80fd814c2d9f1a49e8453c729bd65ac229c94fb
eason <30045503+Eason0729@users.noreply.github.com>
2023-12-11 21:29:59 +0800

    perf: :zap: simple profiling

--- backend/src/endpoint/token.rs
+++ backend/src/endpoint/token.rs
@@ -5,6 +5,7 @@ use super::tools::*;
 
 use crate::grpc::backend::token_set_server::*;
 use crate::grpc::backend::*;
+use crate::grpc::into_chrono;
 use crate::grpc::into_prost;
 
 use entity::token::*;
@@ -50,7 +51,7 @@ impl TokenSet for Arc<Server> {
         Ok(Response::new(Tokens {
             list: tokens.into_iter().map(Into::into).collect(),
         }))
-    }
+    } 
     #[instrument(skip_all, level = "debug")]
     async fn create(&self, req: Request<LoginRequest>) -> Result<Response<TokenInfo>, Status> {
         let db = DB.get().unwrap();
@@ -85,9 +86,9 @@ impl TokenSet for Arc<Server> {
         }
     }
     #[instrument(skip_all, level = "debug")]
-    async fn refresh(&self, req: Request<()>) -> Result<Response<TokenInfo>, Status> {
+    async fn refresh(&self, req: Request<prost_types::Timestamp>) -> Result<Response<TokenInfo>, Status> {
         let db = DB.get().unwrap();
-        let (meta, _, _) = req.into_parts();
+        let (meta, _, payload) = req.into_parts();
 
         if let Some(x) = meta.get("token") {
             let token = x.to_str().unwrap();
@@ -99,7 +100,13 @@ impl TokenSet for Arc<Server> {
                 .map_err(Into::<Error>::into)?
                 .ok_or(Error::NotInDB("user"))?;
 
-            let dur = chrono::Duration::from_std(Duration::from_secs(60 * 60 * 12)).unwrap();
+            let time=into_chrono(payload);
+            let now=chrono::Utc::now().naive_utc();
+            if time<now{
+                return Err(Error::BadArgument("").into());
+            }
+            
+            let dur = time-now;
             self.token.remove(token.to_string()).await?;
 
             let (token, expiry) = self.token.add(&user, dur).await?;
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,9 +1,14 @@
+use tikv_jemallocator::Jemalloc;
+
 pub mod controller;
 pub mod endpoint;
 pub mod grpc;
 pub mod init;
 pub mod macro_tool;
 pub mod server;
+
+#[global_allocator]
+static GLOBAL: Jemalloc = Jemalloc;
 
 #[tokio::main]
 async fn main() {
