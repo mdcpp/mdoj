10b98a2d68fd3b8f3ff4bea9722e9f99e143c9f4
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2024-06-30 14:20:08 +0800

    fix(Grpc): :rotating_light: Ignore linting for generate code

--- grpc/src/lib.rs
+++ grpc/src/lib.rs
@@ -1,15 +1,18 @@
 #[cfg(feature = "wkt")]
+#[allow(clippy::all, non_local_definitions)]
 pub mod backend {
     tonic::include_proto!("oj.backend");
 }
 
 #[cfg(feature = "backend")]
 #[cfg(not(feature = "wkt"))]
+#[allow(clippy::all, non_local_definitions)]
 pub mod backend {
     tonic::include_proto!("oj.backend");
 }
 
 #[cfg(feature = "backend")]
+#[allow(clippy::all, non_local_definitions)]
 pub mod judger {
     tonic::include_proto!("oj.judger");
 }
