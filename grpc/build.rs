ed5b6b7328ae85016a3192cc31fcbf4a92928f28
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2024-07-02 12:00:40 +0800

    feat(Grpc): :sparkles: Separete grpc server client feature

--- grpc/build.rs
+++ grpc/build.rs
@@ -4,10 +4,9 @@ use tonic_build::configure;
 fn main() {
     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
     let descriptor_file = out.join("descriptors.bin");
-    #[cfg(feature = "wkt")]
     configure()
-        // FIXME: this line tell us how build.rs works
-        // .build_server(false)
+        .build_server(cfg!(feature = "server"))
+        .build_client(cfg!(feature = "client"))
         .type_attribute(
             ".",
             r#"#[cfg_attr(feature = "serde", derive(serde::Serialize,serde::Deserialize))]"#,
@@ -16,19 +15,18 @@ fn main() {
         .extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp")
         .extern_path(".google.protobuf.Value", "::prost_wkt_types::Value")
         .file_descriptor_set_path(&descriptor_file)
-        .compile(&["proto/backend.proto", "proto/judger.proto"], &["proto"])
-        .unwrap();
-    #[cfg(feature = "backend")]
-    #[cfg(not(feature = "wkt"))]
-    configure()
-        .type_attribute(
-            ".",
-            r#"#[cfg_attr(feature = "serde", derive(serde::Serialize,serde::Deserialize))]"#,
+        .compile(
+            &[
+                #[cfg(feature = "backend")]
+                "proto/backend.proto",
+                #[cfg(feature = "judger")]
+                "proto/judger.proto",
+            ],
+            &["proto"],
         )
-        .file_descriptor_set_path(&descriptor_file)
-        .compile(&["proto/backend.proto", "proto/judger.proto"], &["proto"])
         .unwrap();
-    #[cfg(all(feature = "wkt", feature = "serde"))]
+
+    #[cfg(feature = "serde")]
     {
         use prost_wkt_build::*;
         let descriptor_bytes = std::fs::read(descriptor_file).unwrap();
