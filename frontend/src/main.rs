fc2f5df2a9c93a281c6df809304a5b4eca99c1f6
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2024-08-21 00:38:26 +0800

    chore(Frontend): :heavy_plus_sign: Add leptos query

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -16,9 +16,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     let conf = get_configuration(None).await?;
     let addr = conf.leptos_options.site_addr;
     // Generate the list of routes in your Leptos App
-    let routes = generate_route_list(App);
+    let routes =
+        leptos_query::with_query_suppression(|| generate_route_list(App));
     println!("listening on http://{}", &addr);
-    init_config().await?;
 
     HttpServer::new(move || {
         let leptos_options: &LeptosOptions = &conf.leptos_options;
