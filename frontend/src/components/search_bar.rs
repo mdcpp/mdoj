34aac010c7fa53b5e8ceee935a2fe64455ce399c
kaiyohugo <41114603+KAIYOHUGO@users.noreply.github.com>
2024-08-23 01:20:27 +0800

    feat(Frontend): :sparkles: close #36
    add search bar

---
+++ frontend/src/components/search_bar.rs
@@ -0,0 +1,21 @@
+use leptos::*;
+use leptos_icons::*;
+use tailwind_fuse::*;
+
+use crate::components::*;
+
+#[component]
+pub fn SearchBar(
+    #[prop(into, optional)] class: String,
+    submit: impl Fn(ev::SubmitEvent, String) + 'static,
+) -> impl IntoView {
+    let search = create_rw_signal("".to_owned());
+    view! {
+        <form on:submit=move |e| submit(e, search.get_untracked()) class=tw_join!("relative",class)>
+            <Input value=search class="flex-grow"></Input>
+            <button type="submit" class="absolute right-4 top-0 h-full">
+                <Icon icon=icondata::BsSearch />
+            </button>
+        </form>
+    }
+}
